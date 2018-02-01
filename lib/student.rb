47a79610c5a586737957053a7a0ca783ad631329
Jennifer Dobak <jdobak1@gmail.com>
2018-02-01 15:43:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,4 +2,15 @@ require_relative "./user.rb"
 
 class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+    def knowledge
+      @knowledge
+    end
 end
